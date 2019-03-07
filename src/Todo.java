ac4c06094f420ea3c66bfd97c005795771659805
Jordan Lanoue <rlanoue@ou.edu>
2019-03-07 11:20:20 -0600

    Added Todo and StackHospital Class

---
+++ src/Todo.java
@@ -0,0 +1,34 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-03-07
+OU ID_______________: 113475042
+OU EMAIL____________: rlanoue@ou.edu
+
+Tasks:
+1. Write StackHospital Class
+2. Write QueueHospital Class
+3. Write PriorityHospital Class
+4. Write SickPerson Class
+5. Write HealthyPerson Class
+6. Test with JUnit
+7. Debug and Clean Up Code
+
+Estimated Time (minutes):
+1. 50
+2. 30
+3. 20
+4. 30
+5. 20
+6. 30
+7. 15
+
+Actual Time (minutes):
+1. 
+2. 
+3. 
+4. 
+5. 
+6. 
+7.  
+
+*/}
